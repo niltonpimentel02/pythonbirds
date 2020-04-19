c4e41779216c0f6a361c61aaba1f9dbe5d453465
Nilton Pimentel <niltonpimentel02@gmail.com>
2020-04-19 16:28:26 -0300

    Implementado motor e direção

--- oo/carro.py
+++ oo/carro.py
@@ -43,28 +43,28 @@ Exemplo:
 >>> direcao = Direcao()
 >>> direcao.valor
 'Norte'
->>> direcao.virar_a_direita()
+>>> direcao.girar_a_direita()
 >>> direcao.valor
 'Leste'
->>> direcao.virar_a_direita()
+>>> direcao.girar_a_direita()
 >>> direcao.valor
 'Sul'
->>> direcao.virar_a_direita()
+>>> direcao.girar_a_direita()
 >>> direcao.valor
 'Oeste'
->>> direcao.virar_a_direita()
+>>> direcao.girar_a_direita()
 >>> direcao.valor
 'Norte'
->>> direcao.virar_a_esquerda()
+>>> direcao.girar_a_esquerda()
 >>> direcao.valor
 'Oeste'
->>> direcao.virar_a_esquerda()
+>>> direcao.girar_a_esquerda()
 >>> direcao.valor
 'Sul'
->>> direcao.virar_a_esquerda()
+>>> direcao.girar_a_esquerda()
 >>> direcao.valor
 'Leste'
->>> direcao.virar_a_esquerda()
+>>> direcao.girar_a_esquerda()
 >>> direcao.valor
 'Norte'
 >>> carro = Carro(direcao, motor)
@@ -91,3 +91,38 @@ Exemplo:
 >>> carro.calcular_direcao()
 'Oeste'
 """
+
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
+
+class Direcao:
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
+    }
+
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
+
+class Motor:
+    def __init__(self):
+        self.velocidade = 0
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        self.velocidade -= 2
+        self.velocidade = max(0, self.velocidade)
